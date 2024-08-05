ef26893b2ed3873691debf5ffc993ecf7a877822
Imoh <jaypee88830@gmail.com>
2024-08-05 14:28:26 +0100

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,11 +10,10 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Imoh Precious </span>
             based in <span className="purple">Lagos, Nigeria.</span>
-            <br/> I have a diploma in Economics from the University of Lagos, I am also a student pursuing a Bachelor's Degree (BSc)
-            in Economics and Finance at Brandon University.
+            <br/> My love for Finance and Technology made me purse a degree in Economics so I can have a blend of the finance world and use that to build awesome products in the tech space. FinTech has always been a field I have always loved.
             <br />
             Additionally, I am currently employed as a software developer at
-            Chekam.com.
+            Chekam.com and Slerp.
             <br />
             I love to use my knowledge of tech to solve real life problems, find solutions, build products.
             <br />
@@ -26,7 +25,7 @@ function AboutCard() {
               <ImPointRight /> Playing Table Tennis
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Conducting research to get new information I might never use, like I can identify at least 150 flags out of 194 countries in the world for no reason.
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
